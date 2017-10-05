222f4d0031abb6ef7e544589df2b45066b228059
Albert <Albert@Albert-HP>
2017-10-05 21:17:21 +0200

    garcia

--- Alex.php
+++ Alex.php
@@ -17,7 +17,7 @@ and open the template in the editor.
             }
         
             public function cognom() {
-                return "cabello";
+                return "garcia";
             }
             
             public function color() {
